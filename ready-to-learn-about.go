0225d3a7bbeb7ee423b34ef8f71d88051b9285b4
RZFeeser <rzfeeser@users.noreply.github.com>
2021-11-29 18:05:26 +0000

    going to lunch day 1

---
+++ ready-to-learn-about.go
@@ -0,0 +1,24 @@
+/* Packages
+   Every Go program is made up of packages.
+   Programs start running in the package main.
+   This program uses the packages with import paths "fmt" and "math/rand"
+*/
+
+// Programs start running in package "main"
+package main
+
+// This program uses the packages with import paths "fmt" and "math/rand"
+import (
+    "fmt"
+    "math/rand"
+    "time"
+)
+
+// Creating a function main()
+func main() {
+    rand.Seed(time.Now().UnixNano())
+    fmt.Println("Looks like you're ready to learn GoLang with Alta3 Research")
+    fmt.Println("My favorite number is 42")
+    fmt.Println("However, I grab random numbers as well", rand.Intn(10))
+}
+
